f758ef5631ccb244d8519e989030873ca0795c46
dj <darrellbrand@gmail.com>
2024-09-27 11:55:44 -0700

    reduce code

--- app/api/getToken/route.tsx
+++
@@ -1,8 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { setToken, TokenResponse, getToken } from '../../home/actions';
-import { NextRequest, NextResponse } from 'next/server';
-
-export async function GET(request : NextRequest) {
-    
-   
-}
