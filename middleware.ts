1d48d50d41742c487c49db4a12d5d8f8c5a6004c
dj <darrellbrand@gmail.com>
2025-04-22 15:43:58 -0700

    wip

--- middleware.ts
+++ middleware.ts
@@ -1,23 +1,14 @@
 import { auth } from "@/auth"
-import { cookies } from "next/headers"
+
 
 
 import { NextRequest, NextResponse } from 'next/server';
 export { auth as middleware } from "@/auth"
-import { NextApiRequest, NextApiResponse } from 'next';
-import { setToken, TokenResponse } from './app/actions/actions';
-import { error } from 'console';
+
 const username = 'dj';
 const password = 'password';
 const authString = btoa(`${username}:${password}`);
-
-export default auth( (req) => {
-  console.log(req.url + "dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd")
-  if(req.url == "/home"){
-
-  }
-
-})
+ 
 export const config = {
   matcher: ["/inventory", "/((?!api|_next/static|_next/image|favicon.ico|home).*)"],
 }
