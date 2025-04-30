41657d5bddf6873334a1c414cabd45a14c83e55c
dj <darrellbrand@gmail.com>
2025-04-29 23:48:27 -0700

    wip

--- components/MyForm.tsx
+++ components/MyForm.tsx
@@ -23,7 +23,7 @@ export function MyForm() {
 
     <form onSubmit={onSubmit} className=' relative flex w-full md:max-w-7xl   justify-center items-center pt-4'>
       <input placeholder="Enter message" value={value} className='border p-5 rounded-3xl w-full   dark:bg-slate-700/30 bg-slate-200/30 h-full  placeholder-slate-600 dark:placeholder-slate-300 ' onChange={e => setValue(e.target.value)} />
-      <button type="submit" disabled={isLoading} className=' p-5 ml-2 rounded-3xl border h-full flex items-center justify-center'
+      <button type="submit" disabled={isLoading} className='  dark:bg-slate-700/30 bg-slate-200/30 p-5 ml-2 rounded-3xl border h-full flex items-center justify-center'
       >
         <Send className='' size={32}></Send>
       </button>
