dcdc84142acc9c49bb128c9f163805f5148095e3
dj <darrellbrand@gmail.com>
2025-05-02 03:16:17 -0700

    wip

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,7 @@ import { auth } from "@/auth"
 
 
 import { getToken, writeToken } from "./app/actions/actions";
+import { NextResponse } from "next/server";
 
 
 const username = 'dj';
@@ -10,9 +11,12 @@ const password = 'password';
 const authString = btoa(`${username}:${password}`);
 export default auth((req) => {
   console.log("MIDDLEWARE")
-     return  writeToken()
-  
+  if (req.auth) {
+    writeToken()
+  } else {
+    return NextResponse.redirect(new URL('/', req.url));
+  }
 })
 export const config = {
-  matcher: [ "/((?!api|_next/static|_next/image|favicon.ico|home).*)"],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|home).*)"],
 }
