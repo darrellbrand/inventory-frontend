4077062ed59fdf77702f2857888e0ade26c9ba55
dj <darrellbrand@gmail.com>
2025-05-02 23:14:56 -0700

    try this

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import next from "next";
 import { Server } from "socket.io";
 
 const dev = process.env.NODE_ENV !== "production";
-const hostname = "localhost";
+const hostname = "0.0.0.0";
 const port = 3000;
 // when using middleware `hostname` and `port` must be provided below
 const app = next({ dev, hostname, port });
@@ -12,9 +12,7 @@ const handler = app.getRequestHandler();
 app.prepare().then(() => {
   const httpServer = createServer(handler);
   const users = new Map(); // socketId => userData
-  const io = new Server(httpServer, {
-    path: '/socket.io',
-  });
+  const io = new Server(httpServer)
   io.on("connection", (socket) => {
     console.log('Client connected');
     io.emit("users:list", Array.from(users.values()));
