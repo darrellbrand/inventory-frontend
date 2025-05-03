841d15c95037e44129ed45edffc97582e78c8b13
dj <darrellbrand@gmail.com>
2025-05-02 19:25:37 -0700

    debug

--- server.js
+++ server.js
@@ -25,6 +25,22 @@ app.prepare().then(() => {
       io.emit('message', data);
     });
 
+    io.engine.on("connection_error", (err) => {
+      console.log(err.req);      // the request object
+      console.log(err.code);     // the error code, for example 1
+      console.log(err.message);  // the error message, for example "Session ID unknown"
+      console.log(err.context);  // some additional error context
+    });
+    socket.on("connect_error", (err) => {
+      // the reason of the error, for example "xhr poll error"
+      console.log(err.message);
+
+      // some additional description, for example the status code of the initial HTTP response
+      console.log(err.description);
+
+      // some additional context, for example the XMLHttpRequest object
+      console.log(err.context);
+    });
     // Store user info (you can modify this as needed)
     socket.on("user:join", (user) => {
       users.set(socket.id, user); // e.g., { username: "John" }
