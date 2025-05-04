06bb2f6270645fe2c6902d058ce5d7df7857ef2a
dj <darrellbrand@gmail.com>
2025-05-03 23:41:39 -0700

    wip

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,14 +9,15 @@ import { SidebarTrigger } from './ui/sidebar';
 const Navbar = () => {
 
   return (
-   
+
     <div className='fixed top-5 z-20 w-full mx-auto '>
       <nav className='relative max-w-xs md:max-w-xs mx-auto flex gap-2 bg-background  items-center  border rounded-2xl justify-evenly p-1' >
-      <SidebarTrigger className="z-30 md:hidden  bg-background"></SidebarTrigger>
+        <SidebarTrigger className="z-30 md:hidden  bg-background"></SidebarTrigger>
         <a href='/'><p className='font-bold my-2'>Home</p></a>
         {/*  <a href='register'><p className='font-bold my-2'>register</p></a>
         <a href='login'><p className='font-bold my-2'>login</p></a> */}
-        <a href='https://darrellbrand.github.io/profile-site-single/'><p className='font-bold my-2'>About</p></a>
+        <a href='https://darrellbrand.github.io/profile-site-single/' target="_blank"
+          rel="noopener noreferrer"><p className='font-bold my-2'>About</p></a>
         <ModeToggle></ModeToggle>
         <UserNav></UserNav>
       </nav>
