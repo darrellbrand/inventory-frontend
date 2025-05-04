1ceffe04eb519762bb2a94db8e8039274fc6d720
dj <darrellbrand@gmail.com>
2025-05-04 02:38:57 -0700

    wip

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const handler = app.getRequestHandler();
 app.prepare().then(() => {
   const httpServer = createServer(handler); // ✅ Must be defined before using it
   const io = new Server(httpServer, {
+    path: "/socket.io",
     addTrailingSlash: false // ✅ Avoids Heroku 308 redirect issue
   });
   const users = new Map(); // socketId => userData
