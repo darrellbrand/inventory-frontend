db3b2e7427571053e2b2f382f9c661b43949cfa0
dj <darrellbrand@gmail.com>
2025-05-03 22:05:50 -0700

    button

--- components/Events.tsx
+++ components/Events.tsx
@@ -10,7 +10,7 @@ export function Events({ events, userList }: EventsProps) {
     return (
 
 
-        <div className=' relative text-xl pt-16 md:pt-10 text-left h-full  md:h-[780px]   w-full  flex  rounded-2xl overflow-hidden ' >
+        <div className=' relative text-xl pt-20 md:pt-10 text-left h-full  md:h-[780px]   w-full  flex  rounded-2xl overflow-hidden ' >
 
             <div className=" mx-auto flex  flex-col md:flex-row w-full h-full overflow-y-auto overflow-x-hidden gap-1   items-end">
                 <ul className='border  text-slate-100 flex-2  px-4 w-full  md:w-1/3  h-1/6 md:h-full  dark:bg-slate-700/30 bg-slate-200/30  rounded-3xl overflow-x-hidden p-2  md:p-14'>
